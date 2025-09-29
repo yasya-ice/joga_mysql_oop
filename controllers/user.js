a30ce9c70d04f85759265b78811fe49809a98866
Yaroslava Nahorna <yasya74800@gmail.com>
2025-09-29 12:00:00 +0200

    Ülesanne 2: Create user login

--- controllers/user.js
+++ controllers/user.js
@@ -13,7 +13,7 @@ class UserController {
             if (req.body.password.length < 6) {
                 return res.status(400).json({ message: 'Password must be at least 6 characters long' });
             }
-            
+
             const cryptPassword = await bcrypt.hash(req.body.password, 10);
             const userData = {
                 username: req.body.username,
@@ -39,6 +39,45 @@ class UserController {
             res.status(500).json({ message: 'Internal server error' });
         }
     }
+    async login(req, res) {
+        try {
+            const user = await userModel.findOne(req.body.username);
+            if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+                return res.status(401).json({ message: 'Invalid username or password' });
+            }
+
+            req.session.user = {
+                username: user.username,
+                user_id: user.id
+            };
+
+            res.json({ 
+                    message: 'User is logged in', 
+                    user: req.session.user 
+                });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    }
+
+    async logout(req, res) {
+        try {
+            console.log(req.session);
+            if (!req.session) {
+                return res.status(200).json({ message: 'Already logged out' });
+            }
+            req.session.destroy((err) => {
+                if (err) {
+                    return res.status(500).json({ message: 'Could not log out. Please try again.' });
+                }
+                res.json({ message: 'User is logged out' });
+            });
+            console.log(req.session);
+        }   catch (error) {     
+            res.json({ message: 'Internal server error' });
+        }   
+    } 
 }
 
 module.exports = UserController;
--- routers/user.js
+++ routers/user.js
@@ -8,4 +8,12 @@ router.post('/register', (req, res) => {
     userController.register(req, res);
 });
 
+router.post('/login', (req, res) => {
+    userController.login(req, res);
+});
+
+router.post('/logout', (req, res) => {
+    userController.logout(req, res);
+});
+
 module.exports = router;
