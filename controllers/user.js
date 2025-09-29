0d8e42d5195d67e32039a8e1ffbf81acc9595253
Yaroslava Nahorna <yasya74800@gmail.com>
2025-09-29 12:00:00 +0200

    Ülesanne 1: Fix existing user and password limitation errors

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,15 @@ const userModel = new UserModel();
 class UserController {
     async register(req, res) {
         try {
+            const existingUser = await userModel.findOne(req.body.username);
+            if (existingUser) {
+                return res.status(400).json({ message: 'Username already exists' });
+            }
+
+            if (req.body.password.length < 6) {
+                return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+            }
+            
             const cryptPassword = await bcrypt.hash(req.body.password, 10);
             const userData = {
                 username: req.body.username,
--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ class UserModel extends BaseSQLModel {
     constructor() {
         super('users');
     }
+    async findOne(username) {
+        const result = await super.findOne('username', username);
+        return result;
+    }
 }
 
 module.exports = UserModel;
